b3228e27ee6a6eff8af5f16bd8e076eea79c4c57
Alukard111(Sergey Zhuravlev) <qb59egkw@gmail.com>
2021-06-27 16:05:53 +0300

    Corrected

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -2,10 +2,8 @@ function ucFirst(str) {
   let result = str
 
   if (!result) {
-    return result.toUpperCase()
-  } 
-  
-  result = result[0].toUpperCase() + result.slice(1)
-  return result
-  
+    return result
+  }
+
+  return result[0].toUpperCase() + result.slice(1)
 }
