0c74a5bdfdbfd4d366b767c7e5827517320cad85
Alukard111(Sergey Zhuravlev) <qb59egkw@gmail.com>
2021-07-01 01:53:54 +0300

    This is the second module with tasks. All completed

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,14 @@
 function sumSalary(salaries) {
   // ваш код...
+  // NaN исключается при сравнении 
+
+  let sumSalaryResult = 0
+
+  for (let key in salaries) {
+    if (salaries[key] != Infinity && salaries[key] != -Infinity && salaries[key] > 0 ) {
+      sumSalaryResult += salaries[key]
+    }
+  }
+
+  return sumSalaryResult
 }
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,20 @@
 let calculator = {
   // ваш код
+  firstValue: null,
+  lastValue: null,
+  read(a, b) {  
+    this.firstValue = a,
+    this.lastValue = b
+  },
+
+  sum() {
+    return this.firstValue + this.lastValue
+  },
+
+  mul() {
+    return this.firstValue * this.lastValue
+  }
+  
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
