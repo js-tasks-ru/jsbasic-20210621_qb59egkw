fa36f6c744467c69d5d8cd040e2fc051ce157236
Alukard111(Sergey Zhuravlev) <qb59egkw@gmail.com>
2021-07-10 21:56:01 +0300

    Fixed 4-module 3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -5,15 +5,18 @@ function highlight(table) {
   for (let i = 1; i < table.rows.length; i++) {
 
     const rowTr = table.rows[i]
-
-    const status = rowTr.lastElementChild
-    const gender = rowTr.lastElementChild.previousElementSibling.textContent
-    const age = rowTr.firstElementChild.nextElementSibling.textContent
     
+    const age = Number(rowTr.cells[1].textContent)
+    const gender = rowTr.cells[2].textContent
+    const status = rowTr.cells[3]
+    
+    if (age < 18) {
+      rowTr.style.textDecoration = 'line-through'
+    }
 
-    Number(age) < 18 ? rowTr.style.textDecoration = 'line-through' : undefined
     gender == "m" ? rowTr.classList.add('male') : rowTr.classList.add('female')
     
+
     if (status.getAttribute('data-available') !== null) {
       status.dataset.available === "true" ? rowTr.classList.add('available') : rowTr.classList.add('unavailable')
     } else {
