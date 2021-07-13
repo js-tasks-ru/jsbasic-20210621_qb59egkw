dd49eed2c19163964e1614f863367db58ba10ae5
Alukard111(Sergey Zhuravlev) <qb59egkw@gmail.com>
2021-07-13 18:14:52 +0300

    Complited 5-module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
   // ваш код...
+  const button = document.querySelector('.toggle-text-button')
+  const div = document.querySelector('#text')
+
+  let hidden = false
+
+  button.addEventListener('click', () => {
+    hidden = !hidden
+    div.hidden = hidden
+  })
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,42 @@
 function initCarousel() {
   // ваш код...
+  const buttonArrowRight = document.querySelector('.carousel__arrow_right')
+  const buttonArrowLeft = document.querySelector('.carousel__arrow_left')
+
+  const carouselInner = document.querySelector('.carousel__inner')
+
+  let carouselInnerWidth = 0
+  const maxCarouselInnerWidth = carouselInner.offsetWidth * 3
+
+
+  const functionButtonArrowRight = () => {
+    carouselInnerWidth += carouselInner.offsetWidth 
+    carouselInner.style.transform = `translateX(-${carouselInnerWidth}px)`
+    checkingVisibilityArrows()
+
+  } 
+
+  const functionButtonArrowLeft = () => {
+    carouselInnerWidth -= carouselInner.offsetWidth 
+    carouselInner.style.transform = `translateX(-${carouselInnerWidth}px)`    
+    checkingVisibilityArrows()
+  } 
+
+  const checkingVisibilityArrows = () => {
+    if (carouselInnerWidth <= 0) {
+      buttonArrowLeft.style.display = 'none'        
+    } else if (carouselInnerWidth >= maxCarouselInnerWidth) {
+      buttonArrowRight.style.display = 'none'    
+    } else {
+      buttonArrowLeft.style.display = ''
+      buttonArrowRight.style.display = '' 
+    }
+  }
+
+
+  buttonArrowRight.addEventListener('click', functionButtonArrowRight)
+  buttonArrowLeft.addEventListener('click', functionButtonArrowLeft)
+
+  checkingVisibilityArrows()
+
 }
